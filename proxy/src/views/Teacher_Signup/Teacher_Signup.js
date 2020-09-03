dd94f0df0b15d69cd531fb784fed2d7db92c638f
Matthew Lyons <matthewwaynelyons@live.com>
2020-09-03 10:32:44 -0700

    Added React hook form, fixed webpack build issue

--- proxy/src/views/Teacher_Signup/Teacher_Signup.js
+++ proxy/src/views/Teacher_Signup/Teacher_Signup.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect, useMemo } from 'react';
 
+import { useForm } from 'react-hook-form';
+
+import * as yup from 'yup';
+
 import { makeRequest } from '../../util';
 
+let emailSchema = yup.string().email();
+
 export default function Teacher_Signup() {
+  const { register, handleSubmit, errors } = useForm({
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit'
+  });
+
   const [success, setSuccess] = useState(false);
   const [responseErrors, setResponseErrors] = useState([]);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    let data = {
-      title: e.target[0].value,
-      firstName: e.target[1].value,
-      lastName: e.target[2].value,
-      email: e.target[3].value
-    };
+  const onSubmit = (data) => {
     console.log(data);
     makeRequest('post', '/teachers', data)
       .then((response) => {
         setSuccess(true);
       })
       .catch((err) => {
-        console.log(err);
         setResponseErrors(err.errors);
       });
   };
@@ -39,14 +42,19 @@ export default function Teacher_Signup() {
           </div>
 
           <form
-            onSubmit={onSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             className="TextCenter StandardForm"
             style={{ display: 'grid', gridRowGap: '1em' }}
           >
             <div className="Grid Gap Third">
               <div>
                 <label htmlFor="title">Title</label>
-                <select className="input" id="title" name="title">
+                <select
+                  className="input"
+                  id="title"
+                  name="title"
+                  ref={register}
+                >
                   <option value="Mr">Mr</option>
                   <option value="Mrs">Mrs</option>
                   <option value="Ms">Ms</option>
@@ -60,20 +68,26 @@ export default function Teacher_Signup() {
                 <input
                   type="text"
                   name="firstName"
-                  className={'input'}
+                  className={errors.firstName ? 'input warning' : 'input'}
                   id="firstName"
-                  required
+                  ref={register({ required: 'First Name is required' })}
                 />
+                <label className="WarningLabel" htmlFor="firstName">
+                  {errors.firstName?.message}
+                </label>
               </div>
               <div>
                 <label htmlFor="lastName">Last Name</label>
                 <input
                   type="text"
                   name="lastName"
-                  className={'input'}
+                  className={errors.lastName ? 'input warning' : 'input'}
                   id="lastName"
-                  required
+                  ref={register({ required: 'Last Name is required' })}
                 />
+                <label className="WarningLabel" htmlFor="lastName">
+                  {errors.lastName?.message}
+                </label>
               </div>
             </div>
 
@@ -82,10 +96,20 @@ export default function Teacher_Signup() {
               <input
                 type="text"
                 name="email"
-                className={'input'}
+                className={errors.email ? 'input warning' : 'input'}
                 id="email"
-                required
+                ref={register({
+                  required: 'Email is required',
+                  validate: {
+                    isEmail: (value) => emailSchema.isValidSync(value)
+                  }
+                })}
               />
+              <label className="WarningLabel" htmlFor="email">
+                {errors.email?.type === 'isEmail'
+                  ? 'Invalid Email'
+                  : errors.email?.message}
+              </label>
             </div>
 
             <button type="submit" className="btn btn-primary" id="Submit">
