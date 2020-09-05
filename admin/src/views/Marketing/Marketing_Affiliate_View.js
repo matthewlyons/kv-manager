d66e0a4fa4e77cce45b2cf5106ab696384a404d1
Matthew Lyons <matthewwaynelyons@live.com>
2020-09-04 20:18:17 -0700

    View Affiliate details

--- admin/src/views/Marketing/Marketing_Affiliate_View.js
+++ admin/src/views/Marketing/Marketing_Affiliate_View.js
@@ -1,30 +1,75 @@
 import React, { useEffect, useState } from 'react';
+import _ from 'underscore';
+import moment from 'moment';
 
-import { Page, Layout } from '@shopify/polaris';
+import {
+  Page,
+  Layout,
+  SkeletonPage,
+  Heading,
+  Card,
+  TextStyle,
+  ResourceList
+} from '@shopify/polaris';
 import { makeRequest } from '../../util';
 
-export default function Marketing_Affiliate_View() {
+export default function Marketing_Affiliate_View(props) {
   const [affiliate, setAffiliate] = useState({});
+  const [orders, setOrders] = useState({});
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    makeRequest('GET', '/affiliate').then((data) => {
-      setAffiliate(data);
-    });
+    makeRequest('GET', `/affiliate/${props.match.params.id}`).then(
+      (response) => {
+        setAffiliate(response.dbAffiliate);
+        setOrders(response.orders);
+        setLoading(false);
+      }
+    );
   }, []);
   return (
-    <Page
-      full-width
-      separator
-      title="Products"
-      breadcrumbs={[
-        {
-          content: 'Back',
-          url: '/Marketing/Affiliate'
-        }
-      ]}
-    >
-      <Layout>
-        <h1>Hello from Marketing_Affiliate_View</h1>
-      </Layout>
-    </Page>
+    <React.Fragment>
+      {loading ? (
+        <SkeletonPage primaryAction></SkeletonPage>
+      ) : (
+        <Page
+          full-width
+          separator
+          title={affiliate.name}
+          breadcrumbs={[
+            {
+              content: 'Back',
+              url: '/Marketing/Affiliate'
+            }
+          ]}
+        >
+          <Layout>
+            <Layout.Section secondary>
+              <Heading>Orders</Heading>
+            </Layout.Section>
+            <Layout.Section>
+              <Card>
+                <ResourceList
+                  resourceName={{ singular: 'customer', plural: 'customers' }}
+                  items={orders}
+                  renderItem={(item) => {
+                    const { orderNumber, totalEarned } = item;
+                    return (
+                      <ResourceList.Item>
+                        <h3>
+                          <TextStyle variation="strong">
+                            Earned: ${totalEarned}
+                          </TextStyle>
+                        </h3>
+                        <div>Order: {orderNumber}</div>
+                      </ResourceList.Item>
+                    );
+                  }}
+                />
+              </Card>
+            </Layout.Section>
+          </Layout>
+        </Page>
+      )}
+    </React.Fragment>
   );
 }
