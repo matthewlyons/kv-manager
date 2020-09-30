865c332b3bac927567da0dc8627eb283db89d5b1
Matthew Lyons <matthewwaynelyons@live.com>
2020-09-30 12:58:42 -0700

    App Visit Schema

--- models/App_Visit.js
+++ models/App_Visit.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const App_Visit_Schema = new Schema({
-  route: { String },
-  ipAddress: { String }
+  route: String,
+  ipAddress: String
 });
 
 module.exports = App_Visit = mongoose.model('appVisit', App_Visit_Schema);
