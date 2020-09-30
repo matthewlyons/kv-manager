afe33e7f541dc215f18df80a52fbfd244a70e988
Matthew Lyons <matthewwaynelyons@live.com>
2020-09-30 12:31:35 -0700

    App Vists

--- routes/proxy/api/old/index.js
+++ routes/proxy/api/old/index.js
@@ -5,19 +5,10 @@ const App_Visit = require('../../../../models/App_Visit');
 
 // Save old app visit to db
 router.post('/', async (req, res) => {
+  console.log('App Visit');
+  console.log(req.body);
+  console.log(req.headers);
   res.send('Done');
-  let { route } = req.body;
-  let ipAddress =
-    req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-  console.log(route);
-  console.log(typeof route);
-  console.log(ipAddress);
-  console.log(typeof ipAddress);
-  let visit = new App_Visit({ route, ipAddress });
-  visit.save().catch((err) => {
-    console.log(err);
-  });
 });
 
 module.exports = router;
