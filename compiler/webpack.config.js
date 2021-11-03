381a6e8b7afc404e12cc92da67707173ecd9d940
Matthew Lyons <matthewwaynelyons@live.com>
2021-11-03 12:33:45 -0700

    Webpack

--- compiler/webpack.config.js
+++ compiler/webpack.config.js
@@ -103,6 +103,17 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
       }
     ]
   }
--- proxy/src/views/Customizer/components/SelectedProductDisplay.js
+++ proxy/src/views/Customizer/components/SelectedProductDisplay.js
@@ -4,8 +4,8 @@ export default function SelectedProductDisplay({ image, title, subheading }) {
   return (
     <div>
       <img src={image} className="DisplayImage" />
-      <p style={{ fontSize: '12px' }}>{title}</p>
-      <p style={{ fontSize: '12px' }}>{subheading}</p>
+      <p style={{ fontSize: '12px', lineHeight: '12px' }}>{title}</p>
+      <p style={{ fontSize: '12px', lineHeight: '12px' }}>{subheading}</p>
     </div>
   );
 }
